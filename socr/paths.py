9ca7db4b5e14149c50a7435caabebe9af59adb3e
p0wer0n <p0wer0n@users.noreply.github.com>
2020-03-20 21:12:12 -0400

    Fix pathing on Mac

--- socr/paths.py
+++ socr/paths.py
@@ -1,6 +1,7 @@
 import xdg.BaseDirectory
 import sys
 import os
+from config import Config
 
 
 _data_dir   = xdg.BaseDirectory.save_data_path('socr')
@@ -8,11 +9,13 @@ _config_dir = xdg.BaseDirectory.save_config_path('socr')
 TEMP_FILE   = '/tmp/socr-temp-file'
 PICKLE_PATH = os.path.join(_data_dir, 'screenshots.pickle')
 sys.path.append(_config_dir)
-try:
-    config = __import__('config')
-except ModuleNotFoundError:
-    print('config file not found', file=sys.stderr)
-    sys.exit(1)
-SCREENSHOT_DIR = os.path.expanduser(config.SCREENSHOT_DIR)
-_screenshot_ext = config.SCREENSHOT_EXT
+config_file = _config_dir + '/config.py'
+config_parsed = Config(config_parsed)
+#try:
+#    config = __import__('config')
+#except ModuleNotFoundError:
+#    print('config file not found', file=sys.stderr)
+#    sys.exit(1)
+SCREENSHOT_DIR = os.path.expanduser(config_parsed.SCREENSHOT_DIR)
+_screenshot_ext = config_parsed.SCREENSHOT_EXT
 SCROT_PATTERN  = os.path.join(SCREENSHOT_DIR, "*." + _screenshot_ext)
